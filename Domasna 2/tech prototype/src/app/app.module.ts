1cc1b187292900e973784b5c6c9454d406310113
ivonamitovska <ivonamitovska55@gmail.com>
2020-12-06 17:45:26 +0100

    created tech prototype

---
+++ Domasna 2/tech prototype/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { RestaurantComponent } from './restaurant/restaurant/restaurant.component';
+import { HeaderComponent } from './header/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    RestaurantComponent,
+    HeaderComponent,
+   
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatCheckboxModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
